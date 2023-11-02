a02d8e7e82c42e12dfa129db3b71dfd467985d71
mfaisalnazari <mfaisalnazari@gmail.com>
2023-11-02 10:57:06 -0600

    Completed Assesment.

--- Vehicle.UnitTests/VehicleTests.cs
+++ Vehicle.UnitTests/VehicleTests.cs
@@ -18,10 +18,33 @@ namespace Vehicle.UnitTests
         {
             Vehicle andysBike = new Vehicle(2, "Blue");
 
-            var expected = "This Blue vehicle has 2 wheels and has driven 0 miles";
+            var expected = "This Blue vehicle has 2 wheels, and has driven 0 miles.";
+            Assert.Equal(expected, andysBike.Summary());
+
+        }
+        [Fact]
+        public void Drive_AddsMiles()
+        {
+            Vehicle megansCar = new Vehicle(4, "Green");
+            megansCar.Drive();
+            var mile = 5;
+            Assert.Equal(mile, megansCar.MilesDriven);
+
 
 
         }
+        [Fact]
+        public void Paint_ChangesColor()
+        {
+            Vehicle megansCar = new Vehicle(4, "Green");
+                megansCar.Paint("Black");
+            Assert.Equal("Black",megansCar.Color);
+
+
+
+
+        }
+
 
         // Add more tests here!
     }
